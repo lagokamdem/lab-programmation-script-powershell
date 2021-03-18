e4b016426b72e944e9f42bbe397010c05fe39eb2
unknown <lagokamdem87@gmail.com>
2021-03-18 17:34:05 -0400

    Bonjour Toronto ! tu as 35 ans.

--- b000000000-fonction.ps1
+++ b000000000-fonction.ps1
@@ -14,8 +14,15 @@
 # Definition de la fonction
 function Stagiaire {
 
+   param (
+        [String]$personneNom,
+        [Int]$personneAge
+    )
+
+    # message de bienvenue 
+    "Bonjour {0} ! Tu as {1} ans." -F $personneNom, $personneAge
 }
 
 # Appel de la fonction
-Stagiaire
+Stagiaire Toronto 35
 
