42c0fdf67f9b0c0ca9d6b5a41ce894b2460aebb9
unknown <lagokamdem87@gmail.com>
2021-03-18 17:02:25 -0400

    en pleine force vive

--- b000000000-structure.ps1
+++ b000000000-structure.ps1
@@ -11,7 +11,15 @@
 
 #>
 
-$personneNom = "Alice"
-$personneAge = 35
+[String]$personneNom = "Alice"
+[Int]$personneAge = 35
 
 Write-Host "Bonjour $personneNom, tu as $personneAge ans. "
+
+# Test sur l'age de la personne
+switch ($personneAge) {
+    {$_ -le 25} {Write-Host 'en pleine jeunesse'; break }
+    {$_ -le 35} {Write-Host 'en pleine force vive'; break }
+    {$_ -le 45} {Write-Host 'en pleine maturité'; break }
+    Default {Write-Host 'en pleine serénité'}
+}
